91eda0a3ca4cab1e3fea9146b9d76f24fadf8989
uGuardian <Sean.Heavysege@gmail.com>
2023-06-09 10:38:35 -0600

    Updated IDs method in ReplayManager

--- ReplayManager.gd
+++ ReplayManager.gd
@@ -5,3 +5,10 @@ func init():
 	for index in Global.current_game.players:
 		frames[index] = {}
 		frames["emotes"][index] = {}
+
+func frame_ids():
+	var ids = .frame_ids()
+	for id in frames:
+		if id is int and !ids.has(id):
+			ids.append(id)
+	return ids
